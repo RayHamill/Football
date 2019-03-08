f09a0dce108838f15b7cf47cc9dd099e7353a648
RayHamill <rayhamill@hotmail.com>
2019-03-08 14:15:09 +0000

    Added expected goals code. Updated folder format.

---
+++ expected_goals/expected_goals_model.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+path = 'C:/Users/Ray/Desktop/Football Data/Opta Data/Football/'
+
+
+def tune_parameters(training_set, training_labels):
+
+    param_grid = {
+        'bootstrap': [True],
+        'max_depth': [70, 80, 90, 100],
+        'max_features': ['auto'],
+        'min_samples_leaf': [1, 2],
+        'min_samples_split': [4, 5, 6],
+        'n_estimators': [400, 600, 800]
+    }
+
+    rf = RandomForestClassifier()
+    grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2)
+    grid_search.fit(training_set, training_labels.values.ravel())
+    return grid_search.best_params
+
+
+def train_xg_model(shot_df):
+
+    features = ['distance', 'angle', 'big_chance', 'header', 'from_corner', 'from_fk', 'free_kick', 'fast_break',
+                'from_cross', 'from_through_ball']
+    data = shot_df.loc[:, features]
+    labels = shot_df.loc[:, ['goal']]
+
+    training_set, test_set, training_labels, test_labels = train_test_split(data, labels, test_size=0.3)
+    params = tune_parameters(training_set, training_labels)
+    rf_classifier = RandomForestClassifier(bootstrap=True, max_depth=params['max_depth'],
+                                           max_features=params['max_features'],
+                                           min_samples_leaf=params['min_samples_leaf'],
+                                           min_samples_split=params['min_samples_split'],
+                                           n_estimators=params['n_estimators'], oob_score=True)
+    rf_classifier.fit(training_set, training_labels.values.ravel())
+
+    preds = pd.DataFrame(rf_classifier.predict_proba(test_set)).iloc[:, 1]
+    rmse = np.sqrt(((preds - test_labels['goal'].reset_index(drop=True)) ** 2).mean())
+    print('RMSE: '+str(round(rmse, 3)))
+
+    feature_importance = rf_classifier.feature_importances_
+    print('\nFeature Importance:')
+    for i in range(0, len(features)):
+        print(features[i]+': '+str(round(feature_importance[i], 3)))
+
+    with open("expected_goals.pkl", 'wb') as file:
+        pickle.dump(rf_classifier, file)
+
+
+if __name__ == '__main__':
+
+    df = pd.read_csv(path+'data/shots_df.csv')
+    train_xg_model(df)
+
+# xG model analysis
+# what leagues to train on?
+# how does it perform in different leagues? etc
+# for now while sharing code i can be discreet by just showing shot_df
---
+++ similarity_scores/similarity_scores.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.decomposition import PCA
+
+all_comps = [2, 4, 6, 9, 10, 1, 3, 7, 11, 12]
+big5_comps = [2, 4, 6, 9, 10]
+other_comps = [1, 3, 7, 11, 12]
+path = 'C:/Users/Ray/Desktop/Football Data/Opta Data/Football/'
+
+similarity_stats = ['shots', 'xG', 'goals', 'key_passes', 'set_piece_key_passes', 'op_xA', 'passes',
+                    'pass_completion_pc', 'crosses', 'cross_completion_pc', 'progressive_passes',
+                    'deep_progression_passes', 'progressive_carries', 'deep_progression_carries', 'touches', 'carries',
+                    'op_pass_box_progressions', 'box_progression_carries',  'Successful Tackles', 'Tackle Percentage',
+                    'Interceptions', 'Ball Recoveries', 'Successful Aerials', 'Aerial Percentage',
+                    'Successful Take Ons', 'Take On Percentage', 'turnovers', 'touches_in_box', 'touch_width',
+                    'touch_depth', 'average_carry_distance', 'average_pass_distance', 'average_progression_distance',
+                    'average_cross_depth', 'headed_shot_percentage']
+
+
+# Standardizes data and applies PCA to deal with collinearity.
+# Returns new df with principle components accounting for 95% of variance, as well as index containing player info.
+def apply_pca(df, year, comps):
+
+    df = df[(df['mins'] > 800) & (df['year'] >= year) & (df['tournament_id'].isin(comps))].set_index(
+        ['player', 'team', 'year', 'age', 'tournament_id']).drop('mins', axis=1).fillna(0)
+    # standardizes performance metrics
+    x = StandardScaler().fit_transform(df)
+    df = pd.DataFrame(x, columns=df.columns, index=df.index)
+
+    # applies PCA
+    pca = PCA(0.95)
+    principal_components = pca.fit_transform(x)
+    pca_df = pd.DataFrame(principal_components, index=df.index)
+    index = pca_df.index
+    # creates new index with string containing player name and year
+    pca_df.index = pca_df.index.map('{0[0]} {0[2]}'.format)
+
+    return pca_df, index
+
+
+# Calls apply_pca, calculates distance of all players from given player, then scales between 0 (furthest point) and 1
+# Filters based on input parameters, saves csv file and
+def calculate_similarity(player, df, comps=all_comps, year=19, max_age=45):
+
+    pca_df, index = apply_pca(df, year, comps)
+    # p1 contains metrics from target player
+    p1 = np.array(pca_df.loc[player])
+    # finds distance from each player to target player
+    dist_df = pd.DataFrame(pca_df.apply(lambda x: np.linalg.norm(p1 - np.array(x)), axis=1), columns=['distance'])
+    dist_df.index = index
+    dist_df = dist_df.reset_index()
+    # adds paths to crest images for plotting later
+    team_crests = pd.read_csv(path+'data/team_crests.csv', index_col=0)
+    dist_df = pd.merge(dist_df, team_crests, how='left', left_on='team', right_on='team').set_index(['player', 'team'])
+
+    # scales distances between 0 and 1
+    scaler = MinMaxScaler()
+    dist_df['similarity'] = 1 - scaler.fit_transform(np.array(dist_df['distance']).reshape(-1, 1))
+    dist_df = dist_df.drop('distance', axis=1)
+    dist_df = dist_df[
+        (dist_df['age'] <= max_age) & (dist_df['year'] >= year) &
+        (dist_df['tournament_id'].isin(comps))].sort_values('similarity', ascending=False).head(20)
+    dist_df.to_csv(path+'data/player_similarity.csv')
+
+    return dist_df['similarity']
+
+
+if __name__ == '__main__':
+
+    player_stats = pd.read_csv(path+'data/player_stats.csv')
+    player_string = 'Lionel Messi 19'
+    comps = all_comps
+    df = player_stats[similarity_stats+['mins', 'year', 'player', 'team', 'age','tournament_id']].reset_index(drop=True)
+    similarity = calculate_similarity(player_string, df, comps)
+    print(similarity.head(20))
+
+# TODO: Add docstrings & uniqueness code.
